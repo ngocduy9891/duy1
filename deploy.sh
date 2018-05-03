eb5c045a6f512fe532a756028a40885360812ad1
ngoc duy tran <36229133+ngocduy9891@users.noreply.github.com>
2018-05-03 10:54:01 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ EB_BUCKET=ngocduy-deploy-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region ap-southeast-1
-aws elasticbeanstalk create-application-version --application-name ngocduy --auto-create-application \
+aws elasticbeanstalk create-application-version \
+    --application-name ngocduy \
     --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
     --region ap-southeast-1
