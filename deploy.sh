666ef434da89151e0489d9e0930dd6e24d7405b0
ngoc duy tran <36229133+ngocduy9891@users.noreply.github.com>
2018-05-04 09:56:05 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ SHA1=$1
 
 # Push image to ECR
 $(aws ecr get-login --region ap-southeast-1)
-docker push 012881927014.dkr.ecr.ap-southeast-1.amazonaws.com/docker:$SHA1
+docker tag app 012881927014.dkr.ecr.ap-southeast-1.amazonaws.com/docker:prod         
+docker push 012881927014.dkr.ecr.ap-southeast-1.amazonaws.com/docker:prod
 
 # Create new Elastic Beanstalk version
 EB_BUCKET=ngocduy-deploy-bucket
